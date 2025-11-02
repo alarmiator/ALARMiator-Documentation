b092c05da165d3ecfbdbebc1d74807e483913c62
Darren Fürst <darren.fuerst@pm.me>
2025-11-02 22:10:39 +0100

    add social card image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="ALARMiator Zusatzalarmierung und Organisationsverwaltung">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
